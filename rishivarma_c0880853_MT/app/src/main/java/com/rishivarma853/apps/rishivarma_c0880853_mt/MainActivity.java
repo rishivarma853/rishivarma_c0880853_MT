88b7b7383b949888e77a810c545dcd4f54e9b53d
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-12-05 19:04:23 -0500

    Created the Project in Android Studio

---
+++ rishivarma_c0880853_MT/app/src/main/java/com/rishivarma853/apps/rishivarma_c0880853_mt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rishivarma853.apps.rishivarma_c0880853_mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
